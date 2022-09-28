7bbf69b29c0db1b285b2432d59665da05ec6ce67
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2022-09-28 08:35:43 +0300

    Added Login & Signup + styles added 2 routes as well

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="auth">
+    <form @submit.enter.prevent="handleSubmit">
+      <h1>Login</h1>
+      <input type="email" placeholder="email" required />
+      <input type="password" placeholder="password" required />
+      <button>Sign up</button>
+      <p class="text">
+        Don't have an account yet?
+        <router-link class="link" :to="{ name: 'signup' }">Login</router-link>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
